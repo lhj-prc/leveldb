1b2a0aa3888f6b52468d1c0ec0d569ef3296cdf4
huaijun lu <hy_luhuaijun@163.com>
2025-01-27 08:35:11 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -13,14 +13,18 @@ int main(int argc, char* argv[])
     Status status = DB::Open(options, "/tmp/testdb", &db);
     assert(status.ok());
 
+    // r/w option
+    ReadOptions read_option;
+    WriteOptions write_option;
+
     // put, get, delete
-    status = db->Put(WriteOptions(), "hello", "LevelDB");
+    status = db->Put(write_option, "hello", "LevelDB");
     assert(status.ok());
     std::string value;
-    status = db->Get(ReadOptions(), "hello", &value);
+    status = db->Get(read_option, "hello", &value);
     assert(status.ok());
     std::cout << value << std::endl;
-    status = db->Delete(WriteOptions(), "hello");
+    status = db->Delete(write_option, "hello");
     assert(status.ok());
 
     // batch
@@ -28,12 +32,11 @@ int main(int argc, char* argv[])
     batch.Delete("k1");
     batch.Put("k2", "v2");
     batch.Put("k3", "v3");
-    status = db->Write(WriteOptions(), &batch);
+    status = db->Write(write_option, &batch);
     assert(status.ok());
 
     // iterator
-    ReadOptions option;
-    Iterator* it = db->NewIterator(option);
+    Iterator* it = db->NewIterator(read_option);
     for (it->SeekToFirst(); it->Valid(); it->Next()) {
         std::cout << it->key().ToString() << ": "  << it->value().ToString() << std::endl;
     }
@@ -41,6 +44,11 @@ int main(int argc, char* argv[])
         std::cout << it->key().ToString() << ": "  << it->value().ToString() << std::endl;
     }
     delete it;
+
+    // snapshot
+    const Snapshot* sp = db->GetSnapshot();
+    read_option.snapshot = sp;
+    db->ReleaseSnapshot(sp);
 
     delete db;
     return 0;
